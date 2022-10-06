78e5d55e7908c88d55e58c50f6d986320f4d46a3
johansantri <johansantri@gmail.com>
2022-10-06 13:03:36 +0700

    update category

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 admin.site.register(Category)
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_display = ('title', 'slug', 'author', 'publish', 'status','category')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
