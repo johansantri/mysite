c031118a61f537cd64e356b054777a93ee5e02ad
johan <johansantri@gmail.com>
2024-10-08 10:28:21 +0700

    default

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,11 +113,35 @@ USE_I18N = True
 
 USE_TZ = True
 
+#login email and verfi email
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "homepage"
+LOGOUT_REDIRECT_URL = "login"
 
+# Setting up Email Backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+
+# Email Host Email Address
+EMAIL_HOST_USER = 'indonesialokal@gmail.com'
+
+# Email Host App Password
+EMAIL_HOST_PASSWORD = 'vdee kfzo dyvk idsa'
+EMAIL_USE_TLS = True
+
+# Default email address to use for various automated correspondence from the site managers.
+DEFAULT_FROM_EMAIL = 'Django Test Email Address'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
