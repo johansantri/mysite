8a63084a40d0bec4e84889a3b202a9f3f0e9b2d3
johan <johansantri@gmail.com>
2024-10-23 12:18:22 +0700

    up

--- courses/forms.py
+++ courses/forms.py
@@ -18,17 +18,13 @@ class CourseForm(forms.ModelForm):
 
         widgets ={
         "course_name" : forms.TextInput(attrs={"placeholder":"full stack d","class":"form-control","oninput":"listingslug(value)"}),
-        "slug" : forms.TextInput(attrs={"placeholder":"full stack","class":"form-control",'type':'hidden'}),
+        "slug" : forms.HiddenInput(attrs={"placeholder":"full stack","class":"form-control","type":"hidden",'maxlength':'10'}),
         "course_number" : forms.TextInput(attrs={"placeholder":"cs201","class":"form-control"}),
         "course_run" : forms.TextInput(attrs={"placeholder":"2023","class":"form-control"}),
-        "slug" : forms.TextInput(attrs={"placeholder":"","class":"form-control"}),
+        
         "org_partner" : forms.Select(attrs={"placeholder":"-","class":"form-control js-example-basic-single","id":"id_org_partner"}),
         "category" : forms.Select(attrs={"placeholder":"-","class":"form-control js-example-basic-single"}),
         
         }
 
-    def clean_course_name(self):
-      data = self.cleaned_data.get('course_name')
-      if data == "":
-         raise forms.ValidationError('this is not an required')
-      return data
+    
