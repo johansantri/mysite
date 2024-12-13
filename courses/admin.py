a6457804a604cbe4e2da585218e3644777a54258
johan <johansantri@gmail.com>
2024-12-13 15:33:11 +0700

    add import export admin course

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from . import models 
-from .models import Partner
+from .models import Partner,Course
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(models.Course)
+class CourseAdmin(ImportExportModelAdmin):
+    list_display = ('course_name', 'course_number', 'course_run', 'category', 'level')  # Adjust based on your fields
+    search_fields = ['course_name', 'course_number']  # Optional: add search functionality
+
+# Register the model with the admin site using the ImportExportModelAdmin
+admin.site.register(Course, CourseAdmin)
 admin.site.register(models.Section)
 class PartnerAdmin(admin.ModelAdmin):
     autocomplete_fields = ['user','author']  # This will apply autocomplete to the 'user' ForeignKey field
