1ec5fd1728ac02f9af8d4b93a8bf4f82bfdb0c25
pakaimata <pakaimata@gmail.com>
2025-03-06 09:30:14 +0700

    add randomize question choice

--- courses/templatetags/custom_filters.py
+++ courses/templatetags/custom_filters.py
@@ -1,8 +1,14 @@
 from django import template
-
+import random
 register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
     """Filter untuk mengambil nilai dari dictionary berdasarkan key"""
     return dictionary.get(key)
+@register.filter
+def randomize(queryset):
+    # Konversi queryset ke list dan acak
+    items = list(queryset)
+    random.shuffle(items)
+    return items
