1f285674956bbaae82a42d933f33df767d594c4f
johan santri <johansantri@gmail.com>
2025-06-02 08:34:32 +0700

    update template blog detail

--- blog/forms.py
+++ blog/forms.py
@@ -143,7 +143,7 @@ class NewCommentForm(forms.ModelForm):
         fields = ['content']
         widgets = {
             'content': forms.Textarea(attrs={
-                'class': 'form-control',
+                'class': 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
                 'placeholder': 'Write your comment here...',
                 'rows': 5,
                 'aria-label': 'Comment',
