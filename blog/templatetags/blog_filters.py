3e054bc14c8f61a72eb0f66c77560ee79f109879
johan santri <johansantri@gmail.com>
2025-07-03 16:37:05 +0700

    update detail blog

--- blog/templatetags/blog_filters.py
+++ blog/templatetags/blog_filters.py
@@ -8,3 +8,10 @@ def div(value, divisor):
         return float(value) / float(divisor)
     except (ValueError, ZeroDivisionError):
         return 0
+    
+@register.filter
+def add_class(field, css_class):
+    """
+    Add CSS classes to a form field's widget.
+    """
+    return field.as_widget(attrs={"class": css_class})
